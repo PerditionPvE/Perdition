b2006810ffd57754b0531f924cf77c2238186741
Alice S <snapp@null.net>
2022-11-10 14:12:29 -0500

    Add `Perdition.OnCreate.DismantleCarBattery`

---
+++ media/lua/shared/PerditionCore.lua
@@ -0,0 +1,14 @@
+
+---@class Perdition
+Perdition = {}
+Perdition.OnCreate = {}
+
+function Perdition.OnCreate.DismantleCarBattery(items, result, player)
+    for i=1, items:size() do
+        local item = items:get(i-1)
+        if item:getType() == "CarBattery" then
+            player:getInventory():addItem("Base.ElectronicsScrap")
+            player:getInventory():addItem("Radio.ElectricWire")
+        end
+    end
+end
