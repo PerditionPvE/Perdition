0f243ee87ab1044cebc5d3fe1571b4c6960bec56
Alice S <snapp@null.net>
2022-11-24 00:12:36 -0500

    `Economy.getBillsByCost`

--- Contents/mods/Perdition - Economy/media/lua/shared/Perdition_EconomyCore.lua
+++ Contents/mods/Perdition - Economy/media/lua/shared/Perdition_EconomyCore.lua
@@ -1,11 +1,27 @@
 ---@class Economy
 Economy = {}
+Economy.bills = {
+    [10000] = "Base.10000bill",
+    [5000] = "Base.5000bill",
+    [1000] = "Base.1000bill",
+    [100] = "Base.100bill",
+    [50] = "Base.50bill",
+    [20] = "Base.20bill",
+    [10] = "Base.10bill",
+    [5] = "Base.5bill",
+    [1] = "Base.Money"
+}
 
----@param player IsoPlayer the player in question
-function checkRoom(player)
-    local cell = player:getCell()
-    local sq = player:getSquare()
-    local region = sq:getIsoWorldRegion()
+---@param cost int the "cost" to calculate
+Economy.getBillsByCost = function(cost)
+    local remainder = cost
+    local result = {}
+    for value, name in pairs(Economy.bills) do
+        local bills = remainder // value -- get the total divisable
+        remainder = remainder % value
+        result[name] = bills
+        print(getItemNameFromFullType(name), "s: ", bills)
+    end
+    return result
 end
-
 --Events.OnPlayerUpdate.Add(checkRoom)
