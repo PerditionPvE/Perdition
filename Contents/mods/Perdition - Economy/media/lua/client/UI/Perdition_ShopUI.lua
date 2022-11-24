71590f906422b86c4c1f656fcf5712bc1d3f6a46
Alice S <snapp@null.net>
2022-11-24 01:43:41 -0500

    indiestone is cringe and Not Fun

--- Contents/mods/Perdition - Economy/media/lua/client/UI/Perdition_ShopUI.lua
+++ Contents/mods/Perdition - Economy/media/lua/client/UI/Perdition_ShopUI.lua
@@ -1,6 +1,64 @@
 ---@class ShopUI : ISPanel
 ShopUI = ISPanel:derive("ShopUI")
 
+local bills = {
+    [10000] = "10000bill",
+    [5000] = "5000bill",
+    [1000] = "1000bill",
+    [100] = "100bill",
+    [50] = "50bill",
+    [20] = "20bill",
+    [10] = "10bill",
+    [5] = "5bill",
+    [1] = "Money"
+}
+
+---@param item Item the iterated item
+local function predicateMoney(item)
+    for _, bill in pairs(bills) do
+        -- gets the base name
+        if item:getFullName() == bill then
+            return true
+        end
+    end
+    return false
+end
+
+---@param cost int the "cost" to calculate
+local function getBillsByCost(cost)
+    local remainder = cost
+    local result = {}
+    for value, name in pairs(bills) do
+        local bills = remainder // value -- get the total divisable
+        remainder = remainder % value
+        result[name] = bills
+        print(getItemNameFromFullType("Base." .. name), "s: ", bills)
+    end
+    return result
+end
+
+---@param player IsoPlayer
+---@return int
+local function getWorth(player)
+    local inv = player:getInventory()
+    local items = inv:getAllEval(predicateMoney)
+    local total = 0
+    for i=0, items:size() - 1 do
+        local item = items:get(i)
+        if item:getFullName() == "Money" then
+            total = total + 1
+            print(total)
+        else
+            for value, name in pairs(bills) do
+                if item:getFullName() == name then
+                    total = total + value
+                    print(total)
+                end
+            end
+        end
+    end
+    return total
+end
 
 local good = {
     r = getCore():getGoodHighlitedColor():getR(),
