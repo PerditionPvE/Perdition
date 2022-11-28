c64a56f5a05caa9dacdfeba160e8cbd439ab280b
Alice <snapp@null.net>
2022-11-28 06:29:03 -0500

    Masterwork fix (#74)
    * Perdition.OnCreate.Masterwork
    
    * Reseal if statement
    
    * Change OnCreate method to Perdition.OnCreate.Masterwork
    
    * update workshop.txt
    
    * not quite 1.0.2 yet
    
    * okay maybe it is 1.0.2 i guess

--- Contents/mods/Perdition/media/lua/server/Perdition_recipe.lua
+++ Contents/mods/Perdition/media/lua/server/Perdition_recipe.lua
@@ -19,6 +19,11 @@ function Perdition.OnCreate.DismantleCarBattery(items, result, player)
     player:getInventory():AddItem("Base.UnusableMetal")
 end
 
+function Perdition.OnCreate.Masterwork(items, result, player)
+    local condPerc = ZombRand(50 + (player:getPerkLevel(Perks.Blacksmith) * 5), 80 + (player:getPerkLevel(Perks.Blacksmith) * 2));
+    result:setCondition(round(result:getCondition() * (condPerc/100)))
+end
+
 function Perdition.GetItemTypes.Sack(scriptItems)
     scriptItems:addAll(getScriptManager():getItemsTag("HoldDirt"))
 end
@@ -28,13 +33,13 @@ function Perdition.GetItemTypes.Jewelry(scriptItems)
     local items = getScriptManager():getAllItems()
     for i=0, items:size()-1 do
         local item = items:get(1)
-        if instanceof(item,"Clothing"
-                and not item:getType() == "Necklace_DogTag"
+        if instanceof(item,"Clothing")
+        and not item:getType() == "Necklace_DogTag"
         then
             if item:isCosmetic() then
                 scriptItems:add(Item)
             end
-        end )
+        end
     end
 end
 -- definitions
