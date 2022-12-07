251afd9a29171f43ec92cbed77f7a80a0b7fb16c
TheQuacc <116894961+TheQuaccer@users.noreply.github.com>
2022-12-08 01:12:31 +0700

    Added Distrubutions again

--- Contents/mods/Perdition/media/lua/server/Items/Perdition_distributions.lua
+++ Contents/mods/Perdition/media/lua/server/Items/Perdition_distributions.lua
@@ -13,47 +13,18 @@ local function preDistributionMerge()
                 "Perdition.SHammerUpgradeManual", 1,
                 "Perdition.CleaverUpgradeManual", 1,
                 "Perdition.ShovelUpgradeManual", 1,
-            }
-        },
-        counter = {
-            rolls = 4,
+                }
+         GigamartHouseElectronics = {
+            rolls = 2,
             items = {
                 "Battery", 2,
                 "BatteryPack4", 1,
                 "BatteryPack8", 1,
-                "BatteryPack12", 0.7,
-                "BatteryPack16", 0.4,
-                "ACDCAdapter", 0.2,
-                "BluePen", 8,
-                "Book", 10,
-                "Eraser", 6,
-                "Magazine", 10,
-                "Notebook", 10,
-                "Paperclip", 20,
-                "PaperclipBox", 0.1,
-                "Pen", 8,
-                "Pencil", 20,
-                "RedPen", 8,
-                "RubberBand", 6,
-                "SheetPaper2", 20,
-            },
-            junk = {
-                rolls = 1,
-                items = {
-                    "Glue", 2,
-                    "HandTorch", 4,
-                    "Radio.RadioBlack", 2,
-                    "Radio.RadioRed", 1,
-                    "Radio.WalkieTalkie2", 1,
-                    "Radio.WalkieTalkie3", 0.5,
-                    "Scissors", 2,
-                    "Scotchtape", 2,
-                    "Torch", 2,
+                "BatteryPack12", 0.8,
+                "BatteryPack4", 0.5,
                 }
-            }
-        },
+            },
         }
-
     }
     for roomDef, list in pairs(PerditionDistributions) do
         -- if not ProceduralDistributions.list[roomDef] then ProceduralDistributions.list[roomDef] = {}; end
